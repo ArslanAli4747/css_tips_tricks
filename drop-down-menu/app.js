b659c80433cd03f22aeee4f84b44aa83141aa459
ArslanAli4747 <105070803+ArslanAli4747@users.noreply.github.com>
2023-02-28 13:11:21 -0800

    added

--- drop-down-menu/app.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu-icon")
-const nav =  document.querySelector(".navigation")
-menu.addEventListener('click',()=>{
-    nav.classList.toggle('active')
-console.log("hi");
-})
